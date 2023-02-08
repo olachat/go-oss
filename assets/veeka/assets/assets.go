2c844b0a8c83611508e77c9d76c7d172502eb1c7
weilson-ola <115454806>
2023-02-08 12:05:48 +0800

    updated assets

--- assets/veeka/assets/assets.go
+++ assets/veeka/assets/assets.go
@@ -24,6 +24,7 @@ const (
 	CommodityBoxRed             = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/commodity_box_red.png"
 	CommodityBoxRedV1           = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/commodity_box_red_v1.png"
 	CommodityBoxRedV2           = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/commodity_box_red_v2.png"
+	CommodityBoxRedV3           = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/commodity_box_red_v3.png"
 	ConfettiPopularityAnimated  = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/confetti_popularity_animated.webp"
 	CpLink0                     = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/cp-link-0.webp"
 	CpLink1                     = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/cp-link-1.webp"
