1e06b15b548257a2a19d92dea10660c11afbec7b
weilson-ola <115454806>
2023-03-20 16:41:45 +0800

    updated assets

--- assets/veeka/assets/chat/assets.go
+++ assets/veeka/assets/chat/assets.go
@@ -2,5 +2,11 @@
 package oss
 
 const (
+	AnniversaryMsgBgEs   = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/chat/anniversary_msg_bg_es.jpg"
+	AnniversaryMsgBgId   = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/chat/anniversary_msg_bg_id.jpg"
+	AnniversaryMsgBgPt   = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/chat/anniversary_msg_bg_pt.jpg"
+	AnniversaryMsgBgTh   = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/chat/anniversary_msg_bg_th.jpg"
+	AnniversaryMsgBgZhCN = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/chat/anniversary_msg_bg_zh_CN.jpg"
+	AnniversaryMsgBgZhTW = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/chat/anniversary_msg_bg_zh_TW.jpg"
 	BgChatGiftCardDialog = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/chat/bg_chat_gift_card_dialog.webp"
 )
