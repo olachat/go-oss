9e63bcf97e5b4330ce1af72ab0fd41a4ead6be6b
weilson-ola <115454806>
2023-03-30 11:14:50 +0800

    updated assets

--- assets/veeka/assets/rechargePopup/assets.go
+++ assets/veeka/assets/rechargePopup/assets.go
@@ -2,7 +2,12 @@
 package oss
 
 const (
-	Type3Banner     = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/recharge_popup/type3_banner.webp"
-	Type3GiftIconBG = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/recharge_popup/type3_gift_icon_BG.webp"
-	Type3Header     = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/recharge_popup/type3_header.webp"
+	Type1Banner        = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/recharge_popup/type1_banner.webp"
+	Type1Header        = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/recharge_popup/type1_header.webp"
+	Type1OldGiftIconBG = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/recharge_popup/type1_old_gift_icon_BG.webp"
+	Type2OldGiftIconBG = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/recharge_popup/type2_old_gift_icon_BG.webp"
+	Type3Banner        = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/recharge_popup/type3_banner.webp"
+	Type3GiftIconBG    = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/recharge_popup/type3_gift_icon_BG.webp"
+	Type3Header        = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/recharge_popup/type3_header.webp"
+	Type3OldGiftIconBG = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/recharge_popup/type3_old_gift_icon_BG.webp"
 )
