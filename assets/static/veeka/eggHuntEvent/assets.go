2a976b887c02e77519b3fc19cb0e3641999b80ba
weilson-ola <115454806>
2023-07-13 19:43:04 +0800

    updated assets

--- assets/static/veeka/eggHuntEvent/assets.go
+++ assets/static/veeka/eggHuntEvent/assets.go
@@ -4,6 +4,7 @@ package oss
 const (
 	EggHuntDesignation55 = "https://partying.oss-ap-southeast-1.aliyuncs.com/static/veeka/egg_hunt_event/egg_hunt_designation_55.png"
 	EggHuntShareAr       = "https://partying.oss-ap-southeast-1.aliyuncs.com/static/veeka/egg_hunt_event/egg_hunt_share_ar.png"
+	EggHuntShareAr1      = "https://partying.oss-ap-southeast-1.aliyuncs.com/static/veeka/egg_hunt_event/egg_hunt_share_ar_1.png"
 	EggHuntShareEn       = "https://partying.oss-ap-southeast-1.aliyuncs.com/static/veeka/egg_hunt_event/egg_hunt_share_en.png"
 	EggHuntShareEn1      = "https://partying.oss-ap-southeast-1.aliyuncs.com/static/veeka/egg_hunt_event/egg_hunt_share_en_1.webp"
 	EggHuntShareEs       = "https://partying.oss-ap-southeast-1.aliyuncs.com/static/veeka/egg_hunt_event/egg_hunt_share_es.png"
@@ -15,6 +16,7 @@ const (
 	EggHuntShareTh       = "https://partying.oss-ap-southeast-1.aliyuncs.com/static/veeka/egg_hunt_event/egg_hunt_share_th.png"
 	EggHuntShareTh1      = "https://partying.oss-ap-southeast-1.aliyuncs.com/static/veeka/egg_hunt_event/egg_hunt_share_th_1.webp"
 	EggHuntShareVi       = "https://partying.oss-ap-southeast-1.aliyuncs.com/static/veeka/egg_hunt_event/egg_hunt_share_vi.png"
+	EggHuntShareVi1      = "https://partying.oss-ap-southeast-1.aliyuncs.com/static/veeka/egg_hunt_event/egg_hunt_share_vi_1.png"
 	EggHuntShareZhCn     = "https://partying.oss-ap-southeast-1.aliyuncs.com/static/veeka/egg_hunt_event/egg_hunt_share_zh_cn.png"
 	EggHuntShareZhCn1    = "https://partying.oss-ap-southeast-1.aliyuncs.com/static/veeka/egg_hunt_event/egg_hunt_share_zh_cn_1.webp"
 	EggHuntShareZhTw     = "https://partying.oss-ap-southeast-1.aliyuncs.com/static/veeka/egg_hunt_event/egg_hunt_share_zh_tw.png"
