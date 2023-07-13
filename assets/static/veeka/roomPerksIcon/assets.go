3656608527a11cd3026486cecc119301497f54bf
weilson-ola <115454806>
2023-07-13 17:47:58 +0800

    updated assets

--- assets/static/veeka/roomPerksIcon/assets.go
+++ assets/static/veeka/roomPerksIcon/assets.go
@@ -6,6 +6,7 @@ const (
 	CpFriendMic      = "https://partying.oss-ap-southeast-1.aliyuncs.com/static/veeka/room_perks_icon/cp_friend_mic.png"
 	FancyAnimation   = "https://partying.oss-ap-southeast-1.aliyuncs.com/static/veeka/room_perks_icon/fancy_animation.png"
 	FlyScreenV1      = "https://partying.oss-ap-southeast-1.aliyuncs.com/static/veeka/room_perks_icon/fly_screenV1.png"
+	RoomLevelUpgrade = "https://partying.oss-ap-southeast-1.aliyuncs.com/static/veeka/room_perks_icon/room_level_upgrade.webp"
 	RoomThemeImmerse = "https://partying.oss-ap-southeast-1.aliyuncs.com/static/veeka/room_perks_icon/room_theme_immerse.png"
 	VideoRoomSticker = "https://partying.oss-ap-southeast-1.aliyuncs.com/static/veeka/room_perks_icon/video_room_sticker.png"
 )
