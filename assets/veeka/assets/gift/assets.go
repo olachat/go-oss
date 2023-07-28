f05afaad71e5f8ecdfdb2a00e1cf3a0ca1524e96
weilson-ola <115454806>
2023-07-28 19:13:43 +0800

    updated assets

--- assets/veeka/assets/gift/assets.go
+++ assets/veeka/assets/gift/assets.go
@@ -4,4 +4,5 @@ package oss
 const (
 	BlindRingRankBg     = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/gift/blind_ring_rank_bg.webp"
 	DiyGiftItemSelectBg = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/gift/diy_gift_item_select_bg.webp"
+	FlowerGiftBox       = "https://partying.oss-ap-southeast-1.aliyuncs.com/veeka/assets/gift/flower_gift_box.png"
 )
